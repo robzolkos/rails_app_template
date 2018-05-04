753eed23897a94d5fac804042415f277dd50d5f0
Rob Zolkos <rob@zolkos.com>
2018-05-05 08:54:24 +1000

    Initial commit

---
+++ app/javascript/js/rails_turbolinks.js
@@ -0,0 +1,4 @@
+import Rails from "rails-ujs";
+import Turbolinks from "turbolinks";
+Rails.start();
+Turbolinks.start();
---
+++ app/javascript/packs/vendor.js
@@ -0,0 +1,4 @@
+import "../js/rails_turbolinks";
+import "../js/rails_activestorage";
+import "../js/jquery";
+import "../js/select2";
